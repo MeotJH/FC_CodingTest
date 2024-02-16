34e779d8ff05a4fa3be6dfe08472432234ea02c9
김진한 <kimjh0417@gsitm.com>
2024-02-16 10:42:42 +0900

    Backjoon1730 판화 실4 BruteForce 강의보고 성공
    강의는 논리적으로 크게 2부분을 나누었는데반해
    나는 그 역할을 1번으로 하려고 하다보니 어려웠다.
    
    논리적으로 나눌 수 있다면 그렇게 하도록 컴퓨터적으로 생각해야 할 것 같다.

--- src/Part1/Bj1730.java
+++ src/Part1/Bj1730.java
@@ -13,25 +13,58 @@ public class Bj1730 {
         String paths  = br.readLine();
         //길 없을때 예외처리
 
-        char[][] routes = new char[range][range];
-
         boolean[][] rangeWidth = new boolean[range][range];
         boolean[][] rangeHeight = new boolean[range][range];
 
         char[] charArray = paths.toCharArray();
-        int x, y = 0;
+        int x = 0, y = 0;
         for (int i = 0; i < paths.length(); i++) {
 
             if(charArray[i] == 'U'){
+                if(x == 0) continue;
 
-
+                rangeHeight[x][y] = true;
+                rangeHeight[x-1][y] = true;
+                x--;
             } else if (charArray[i] == 'D') {
+                if(x == range-1) continue;
 
+                rangeHeight[x][y] = true;
+                rangeHeight[x+1][y] = true;
+                x++;
             }else if (charArray[i] == 'R') {
+                if(y == range-1 ) continue;
 
+                rangeWidth[x][y] = true;
+                rangeWidth[x][y+1]= true;
+                y++;
             }else if (charArray[i] == 'L') {
+                if(y == 0) continue;
 
+                rangeWidth[x][y] = true;
+                rangeWidth[x][y-1] = true;
+                y--;
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < range; i++) {
+            for (int j = 0; j < range; j++) {
+
+                if(rangeHeight[i][j] == rangeWidth[i][j] && rangeHeight[i][j] == true){
+                    sb.append("+");
+                } else if (rangeHeight[i][j] == true) {
+                    sb.append("|");
+                } else if (rangeWidth[i][j] == true) {
+                    sb.append("-");
+                }else{
+                    sb.append(".");
+                }
+
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
